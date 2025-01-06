4d885a9646042143ab6b99b9e29dc4f524bde718
Angelmbx <mirandabl.angel@gmail.com>
2025-01-06 19:12:06 +0100

    refactor(assess_game): Mover assess_game() del main al archivo game_logic.py

--- src/main.py
+++ src/main.py
@@ -1,43 +1,8 @@
 import random
+from game_logic import assess_game
 from constants import  GameAction, GameResult
 from history import update_user_history, predict_user_action
 
-def assess_game(user_action, computer_action):
-
-    game_result = None
-
-    if user_action == computer_action:
-        print(f"User and computer picked {user_action.name}. Draw game!")
-        game_result = GameResult.Tie
-
-    # You picked Rock
-    elif user_action == GameAction.Rock:
-        if computer_action == GameAction.Scissors:
-            print("Rock smashes scissors. You won!")
-            game_result = GameResult.Victory
-        else:
-            print("Paper covers rock. You lost!")
-            game_result = GameResult.Defeat
-
-    # You picked Paper
-    elif user_action == GameAction.Paper:
-        if computer_action == GameAction.Rock:
-            print("Paper covers rock. You won!")
-            game_result = GameResult.Victory
-        else:
-            print("Scissors cuts paper. You lost!")
-            game_result = GameResult.Defeat
-
-    # You picked Scissors
-    elif user_action == GameAction.Scissors:
-        if computer_action == GameAction.Rock:
-            print("Rock smashes scissors. You lost!")
-            game_result = GameResult.Defeat
-        else:
-            print("Scissors cuts paper. You won!")
-            game_result = GameResult.Victory
-
-    return game_result
 
 
 def get_computer_action():
